ff4ede3e384b36519f6b51ebb318e7fbecd8e41d
Edwin Rivera <gaedwin92@gmail.com>
2026-04-09 14:05:56 -0600

    Blog en prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://panelparacuartofrio.com',
   integrations: [sitemap()]
 });
+
