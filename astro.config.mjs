7abb84a6a56d6622339e5bf97936e6203feff200
Edwin Rivera <gaedwin92@gmail.com>
2026-04-10 17:05:23 -0600

    Responsive casi completo, pagina 404 y blog headless WordPress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://panelparacuartofrio.com',
-  base: 'landing_panelparacuartofrio',
   integrations: [sitemap()]
 });
-
